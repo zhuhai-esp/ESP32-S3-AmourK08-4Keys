827e75656cc65a342ff24a464fcad7d694590df9
huzongyao <huzongyao@tangbull.com>
2024-12-17 00:42:11 +0800

    add music play

--- K08-4Keys-I2C-AHT20/src/main.cpp
+++ K08-4Keys-I2C-AHT20/src/main.cpp
@@ -1,10 +1,23 @@
+#include "lgfx.h"
 #include <Adafruit_AHTX0.h>
 
+#define FONT16 &fonts::efontCN_16
+
 Adafruit_AHTX0 aht;
+LGFX tft;
+
+void inline initTFTDevice() {
+  tft.init();
+  tft.setBrightness(60);
+  tft.fillScreen(TFT_BLACK);
+  tft.setFont(FONT16);
+  tft.setColorDepth(8);
+}
 
 void setup() {
   Wire.setPins(1, 2);
   Serial.begin(115200);
+  initTFTDevice();
   Serial.println("Adafruit AHT10/AHT20 demo!");
   if (!aht.begin()) {
     Serial.println("Could not find AHT? Check wiring");
@@ -19,5 +32,9 @@ void loop() {
   aht.getEvent(&humidity, &temp);
   Serial.printf("AHT20传感器测量温度: %.2f摄氏度, 湿度: %.2f%%\n",
                 temp.temperature, humidity.relative_humidity);
+  tft.clear();
+  tft.setCursor(0, 0);
+  tft.printf("AHT20传感器测量温度: %.2f摄氏度, 湿度: %.2f%%\n",
+             temp.temperature, humidity.relative_humidity);
   delay(2000);
 }
--- K08-4Keys-I2C-MPU6050/src/main.cpp
+++ K08-4Keys-I2C-MPU6050/src/main.cpp
@@ -1,10 +1,23 @@
 #include <Adafruit_MPU6050.h>
+#include "lgfx.h"
+
+#define FONT16 &fonts::efontCN_16
 
 Adafruit_MPU6050 mpu;
+LGFX tft;
+
+void inline initTFTDevice() {
+  tft.init();
+  tft.setBrightness(60);
+  tft.fillScreen(TFT_BLACK);
+  tft.setFont(FONT16);
+  tft.setColorDepth(8);
+}
 
 void setup() {
   Wire.setPins(1, 2);
   Serial.begin(115200);
+  initTFTDevice();
   Serial.println("Adafruit MPU6050 test!");
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
@@ -26,6 +39,11 @@ void loop() {
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
     Serial.printf("Accel: [%.2f,%.2f,%.2f], gyro:[%.2f,%.2f,%.2f]\n",
+                  a.acceleration.x, a.acceleration.y, a.acceleration.z,
+                  g.gyro.x, g.gyro.y, g.gyro.z);
+    tft.clear();
+    tft.setCursor(0, 0);
+    tft.printf("Accel: [%.2f,%.2f,%.2f]\ngyro:[%.2f,%.2f,%.2f]\n",
                   a.acceleration.x, a.acceleration.y, a.acceleration.z,
                   g.gyro.x, g.gyro.y, g.gyro.z);
   }
